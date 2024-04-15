f0bb9c0a20553481f70430aa252b40b615cf98da
kirill khlebov <khlebov79@gmail.com>
2024-04-15 14:21:44 +0300

    init: items new function

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -1,20 +1,31 @@
 const BASE_URL = 'http://localhost:3000/item'
 
-const parseResponse = (response)  => response.json()
+const parseResponse = (response) => response.json()
 
 export const itemAPI = {
   getItem: async () => {
     return fetch(BASE_URL).then(parseResponse)
   },
   addItem: async (id, title, count, date) => {
-    return fetch(`${BASE_URL}/${id,  title, count, date}`, {
+    return fetch(`${BASE_URL}/${id, title, count, date}`, {
       body: JSON.stringify({id}),
       method: 'POST'
     }).then(parseResponse)
   },
   deleteItem: async (id) => {
     return fetch(`${BASE_URL}/${id}`, {
-      method: "DELETE"
+      method: 'DELETE',
     }).then(parseResponse)
   }
+}
+
+
+export const getCachedItems = () => {
+  const items = localStorage.getItem('items')
+
+  return items ? JSON.parse(items) : []
+}
+
+export const saveItemsToCache = (id) => {
+  localStorage.setItem('items', JSON.stringify(id))
 }
--- src/functions/item.js
+++ src/functions/item.js
@@ -1,2 +1,67 @@
-import {itemAPI} from "/functions.js"
+import {itemAPI, getCachedItems, saveItemsToCache} from "./functions.js"
+
+const creamShopListElement = document.querySelector('.cream-shop-list')
+
+const cachedItems = getCachedItems()
+let items = cachedItems.length > 0 ? cachedItems  : await itemAPI.getItem()
+
+
+const renderItems = () => {
+
+  creamShopListElement.innerHTML = items.reduce((acc, el) => {
+    const {id, title, count, date} = el
+      const item = `
+      <div class="cream-shop-list__item" data-id="${id}">
+        <div class="cream-shop-list__name">
+          <p class="fw-bold">${id}</p>
+          <div class="cream-shop-list__title fw-medium">${title}</div>
+        </div>
+        <div class="cream-shop-list__name">
+          <span class="fw-medium">${count} шт</span>
+          <span class="fw-medium">${date}</span>
+        </div>
+        <div class="button">
+          <button class="button__delete btn btn-danger">Удалить</button>
+          <button class="button__edit btn btn-primary">Редакт</button>
+        </div>
+      `
+
+    return item + acc
+  }, '')
+}
+
+renderItems()
+
+
+const removeItemsFromDom = (id) => {
+  const itemsElement = document.querySelector(`.cream-shop-list__item[data-id="${id}"]`)
+  itemsElement?.remove()
+}
+
+const deleteItems = (id) => {
+  itemAPI.deleteItem(id)
+    .then(() => {
+      items = items.filter((item) => item.id !== id)
+      removeItemsFromDom(id)
+      saveItemsToCache(items)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+const onItemsListClick = (event) => {
+  const itemDeleteElement = event.target.closest('.button__delete')
+
+  if(!itemDeleteElement) return
+
+  const itemElement = itemDeleteElement.closest('.cream-shop-list__item')
+  const {id} = itemElement.dataset
+
+
+  deleteItems(id)
+}
+
+creamShopListElement.addEventListener('click', onItemsListClick)
+
 
